a79447ded321441c7804eb04d9f7da27e68625fa
Eldy Deines <eldy.deines@outlook.com>
2021-09-03 14:59:31 -0600

    added test cases for filtering and updated readme

--- readme.md
+++ readme.md
@@ -28,4 +28,31 @@ To run the tests:
 - [x] Start up the server (note that, unlike most exercises, we start this server on port 3001).
 - [x] Test the API in Insomnia.
 ### Primary focus and work updates to: 
-- [x] sqlForPartialUpdate -- This function was literally functional.  My work included adding documentation, which required analyzing where this function was used, what inputs were required, how it executed, and what was outputed.  Additionally, I added my own tests for this function. I wrote 13 test cases to test the function's inputs, execution process and outputs.  (See my work on [/helpers/sql.js](/helpers/sql.js) and [/helpers/sql.test.js](/helpers/sql.test.js))
+- [x] sqlForPartialUpdate -- This function was literally functional.  My work included adding documentation, which required analyzing where this function was used, what inputs were required, how it executed, and what was outputed.  Additionally, I added my own tests for this function. I wrote 13 test cases to test the function's inputs, execution process and outputs.  
+See my work: [/helpers/sql.js](/helpers/sql.js) and [/helpers/sql.test.js](/helpers/sql.test.js))
+
+## Part Two: Companies
+### Adding Filtering 
+- [x] Ability to provide a query string for nameLike, minEmployees, maxEmployees
+- [x] Filter capability to find case-insensitive, partial matches for name in companies table.
+- [x] Filter capability to show on companies that are above the minEmployees number provided.
+- [x] Filter capability to show on companies that are below the maxEmployees number provided.
+- [x] If minEmployees is greater than MaxEmployees, app throws an error and provides error message in JSON reply.
+- [x] Ensures no other query parameters are provided. If so, it handles the error.
+- [x] If no query string sent, it still provides all company as needed. 
+- [x] Added comments to help anyone understand what was done
+
+See my work: [/models/company.js](/models/company.js) 
+Specific function to review: 
+
+```
+static async findAll(filters = {})
+```
+### Testing
+- [x] Edge case added for name parameter to check for case-insensitivity and partial matches
+- [x] Test case on all three parameters provided
+- [x] Test case with maxEmployees being smaller than minEmployees
+- [x] Test case with minEmployees and maxEmployees provided
+- [x] Test case with an invalid parameter provided
+
+See my work: [/models/company.test.js](/models/company.test.js) 
--- routes/companies.test.js
+++ routes/companies.test.js
@@ -31,9 +31,9 @@ describe("POST /companies", function () {
 
   test("ok for users", async function () {
     const resp = await request(app)
-        .post("/companies")
-        .send(newCompany)
-        .set("authorization", `Bearer ${u1Token}`);
+      .post("/companies")
+      .send(newCompany)
+      .set("authorization", `Bearer ${u1Token}`);
     expect(resp.statusCode).toEqual(201);
     expect(resp.body).toEqual({
       company: newCompany,
@@ -42,23 +42,23 @@ describe("POST /companies", function () {
 
   test("bad request with missing data", async function () {
     const resp = await request(app)
-        .post("/companies")
-        .send({
-          handle: "new",
-          numEmployees: 10,
-        })
-        .set("authorization", `Bearer ${u1Token}`);
+      .post("/companies")
+      .send({
+        handle: "new",
+        numEmployees: 10,
+      })
+      .set("authorization", `Bearer ${u1Token}`);
     expect(resp.statusCode).toEqual(400);
   });
 
   test("bad request with invalid data", async function () {
     const resp = await request(app)
-        .post("/companies")
-        .send({
-          ...newCompany,
-          logoUrl: "not-a-url",
-        })
-        .set("authorization", `Bearer ${u1Token}`);
+      .post("/companies")
+      .send({
+        ...newCompany,
+        logoUrl: "not-a-url",
+      })
+      .set("authorization", `Bearer ${u1Token}`);
     expect(resp.statusCode).toEqual(400);
   });
 });
@@ -70,31 +70,96 @@ describe("GET /companies", function () {
     const resp = await request(app).get("/companies");
     expect(resp.body).toEqual({
       companies:
-          [
-            {
-              handle: "c1",
-              name: "C1",
-              description: "Desc1",
-              numEmployees: 1,
-              logoUrl: "http://c1.img",
-            },
-            {
-              handle: "c2",
-              name: "C2",
-              description: "Desc2",
-              numEmployees: 2,
-              logoUrl: "http://c2.img",
-            },
-            {
-              handle: "c3",
-              name: "C3",
-              description: "Desc3",
-              numEmployees: 3,
-              logoUrl: "http://c3.img",
-            },
-          ],
+        [
+          {
+            handle: "c1",
+            name: "C1",
+            description: "Desc1",
+            numEmployees: 1,
+            logoUrl: "http://c1.img",
+          },
+          {
+            handle: "c2",
+            name: "C2",
+            description: "Desc2",
+            numEmployees: 2,
+            logoUrl: "http://c2.img",
+          },
+          {
+            handle: "c3",
+            name: "C3",
+            description: "Desc3",
+            numEmployees: 3,
+            logoUrl: "http://c3.img",
+          },
+        ],
     });
-  });
+  }); //end test
+
+  test("parameters testing name, min, and max", async function () {
+    const resp = await request(app).get(`/companies`).query({ "nameLike": "c2", "minEmployees": "1", "maxEmployees": "3" });
+    expect(resp.body).toEqual({
+      companies:
+        [
+          {
+            handle: "c2",
+            name: "C2",
+            description: "Desc2",
+            numEmployees: 2,
+            logoUrl: "http://c2.img",
+          },
+        ],
+    });
+  });// end test
+
+  test("parameters testing name only with case sensitivity and contains", async function () {
+    const resp = await request(app).get(`/companies`).query({ "nameLike": "c2" });
+    expect(resp.body).toEqual({
+      companies:
+        [
+          {
+            handle: "c2",
+            name: "C2",
+            description: "Desc2",
+            numEmployees: 2,
+            logoUrl: "http://c2.img",
+          },
+        ],
+    });
+  });// end test
+
+  test("parameters for min and max employees - no name", async function () {
+    const resp = await request(app).get(`/companies`).query({ "minEmployees": "1", "maxEmployees": "5" });
+    expect(resp.body).toEqual({
+      companies:
+        [
+          {
+            handle: "c2",
+            name: "C2",
+            description: "Desc2",
+            numEmployees: 2,
+            logoUrl: "http://c2.img",
+          },
+          {
+            handle: "c3",
+            name: "C3",
+            description: "Desc3",
+            numEmployees: 3,
+            logoUrl: "http://c3.img",
+          },
+        ],
+    });
+  });//end test
+
+  test("invalid parameter", async function () {
+    const resp = await request(app).get(`/companies`).query({ "numEmployees": "1" });
+    expect(resp.statusCode).toEqual(400);
+  });//end test
+
+  test("parameters where min and max employees is invalid", async function () {
+    const resp = await request(app).get(`/companies`).query({ "minEmployees": "5", "maxEmployees": "2" });
+    expect(resp.statusCode).toEqual(400);
+  });//end test
 
   test("fails: test next() handler", async function () {
     // there's no normal failure event which will cause this route to fail ---
@@ -102,10 +167,15 @@ describe("GET /companies", function () {
     // should cause an error, all right :)
     await db.query("DROP TABLE companies CASCADE");
     const resp = await request(app)
-        .get("/companies")
-        .set("authorization", `Bearer ${u1Token}`);
+      .get("/companies")
+      .set("authorization", `Bearer ${u1Token}`);
     expect(resp.statusCode).toEqual(500);
   });
+
+  //test company 1
+  //test with params (each param)
+  //test result
+
 });
 
 /************************************** GET /companies/:handle */
@@ -148,11 +218,11 @@ describe("GET /companies/:handle", function () {
 describe("PATCH /companies/:handle", function () {
   test("works for users", async function () {
     const resp = await request(app)
-        .patch(`/companies/c1`)
-        .send({
-          name: "C1-new",
-        })
-        .set("authorization", `Bearer ${u1Token}`);
+      .patch(`/companies/c1`)
+      .send({
+        name: "C1-new",
+      })
+      .set("authorization", `Bearer ${u1Token}`);
     expect(resp.body).toEqual({
       company: {
         handle: "c1",
@@ -166,40 +236,40 @@ describe("PATCH /companies/:handle", function () {
 
   test("unauth for anon", async function () {
     const resp = await request(app)
-        .patch(`/companies/c1`)
-        .send({
-          name: "C1-new",
-        });
+      .patch(`/companies/c1`)
+      .send({
+        name: "C1-new",
+      });
     expect(resp.statusCode).toEqual(401);
   });
 
   test("not found on no such company", async function () {
     const resp = await request(app)
-        .patch(`/companies/nope`)
-        .send({
-          name: "new nope",
-        })
-        .set("authorization", `Bearer ${u1Token}`);
+      .patch(`/companies/nope`)
+      .send({
+        name: "new nope",
+      })
+      .set("authorization", `Bearer ${u1Token}`);
     expect(resp.statusCode).toEqual(404);
   });
 
   test("bad request on handle change attempt", async function () {
     const resp = await request(app)
-        .patch(`/companies/c1`)
-        .send({
-          handle: "c1-new",
-        })
-        .set("authorization", `Bearer ${u1Token}`);
+      .patch(`/companies/c1`)
+      .send({
+        handle: "c1-new",
+      })
+      .set("authorization", `Bearer ${u1Token}`);
     expect(resp.statusCode).toEqual(400);
   });
 
   test("bad request on invalid data", async function () {
     const resp = await request(app)
-        .patch(`/companies/c1`)
-        .send({
-          logoUrl: "not-a-url",
-        })
-        .set("authorization", `Bearer ${u1Token}`);
+      .patch(`/companies/c1`)
+      .send({
+        logoUrl: "not-a-url",
+      })
+      .set("authorization", `Bearer ${u1Token}`);
     expect(resp.statusCode).toEqual(400);
   });
 });
@@ -209,21 +279,21 @@ describe("PATCH /companies/:handle", function () {
 describe("DELETE /companies/:handle", function () {
   test("works for users", async function () {
     const resp = await request(app)
-        .delete(`/companies/c1`)
-        .set("authorization", `Bearer ${u1Token}`);
+      .delete(`/companies/c1`)
+      .set("authorization", `Bearer ${u1Token}`);
     expect(resp.body).toEqual({ deleted: "c1" });
   });
 
   test("unauth for anon", async function () {
     const resp = await request(app)
-        .delete(`/companies/c1`);
+      .delete(`/companies/c1`);
     expect(resp.statusCode).toEqual(401);
   });
 
   test("not found for no such company", async function () {
     const resp = await request(app)
-        .delete(`/companies/nope`)
-        .set("authorization", `Bearer ${u1Token}`);
+      .delete(`/companies/nope`)
+      .set("authorization", `Bearer ${u1Token}`);
     expect(resp.statusCode).toEqual(404);
   });
 });
