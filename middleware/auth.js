3c4fda4e84ceecc7335aa04ccaa8d4c791e01863
Eldy Deines <eldy.deines@outlook.com>
2021-09-05 11:10:06 -0600

    added ensure admin authentication function

--- middleware/auth.js
+++ middleware/auth.js
@@ -26,7 +26,7 @@ function authenticateJWT(req, res, next) {
   } catch (err) {
     return next();
   }
-}
+};
 
 /** Middleware to use when they must be logged in.
  *
@@ -40,10 +40,26 @@ function ensureLoggedIn(req, res, next) {
   } catch (err) {
     return next(err);
   }
-}
+};
+
+
+/** Middleware to use when they must be an admin.
+ * 
+ * If not, raises Unauthorized.
+ */
+
+function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user || !res.locals.user.isAdmin) throw new UnauthorizedError();
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+};
 
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin
 };
