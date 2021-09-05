dd0d7cdc88bb609ffd9b5ab39ff62cb5991d2b1c
Eldy Deines <eldy.deines@outlook.com>
2021-09-05 12:30:31 -0600

    updated to accomodate for not a logged in user

--- middleware/auth.js
+++ middleware/auth.js
@@ -64,13 +64,14 @@ function ensureAdmin(req, res, next) {
 
 function ensureAuthUser(req, res, next) {
   try {
-
+    if (!res.locals.user) {
+      throw new UnauthorizedError();
+    }
     if ((res.locals.user.username == req.params.username) || res.locals.user.isAdmin) {
       return next();
     }
-    else {
-      throw new UnauthorizedError();
-    }
+
+    throw new UnauthorizedError();
 
   } catch (err) {
     return next(err);
