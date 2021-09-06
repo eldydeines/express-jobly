7d51eedf810e75e7b890724b42c46d534308eddb
Eldy Deines <eldy.deines@outlook.com>
2021-09-05 20:58:43 -0600

    updated comments

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -192,27 +192,22 @@ describe("GET /companies", function () {
 describe("GET /companies/:handle", function () {
   test("works for anon", async function () {
     const resp = await request(app).get(`/companies/c1`);
-    expect(resp.body).toEqual({
-      company: {
-        handle: "c1",
-        name: "C1",
-        description: "Desc1",
-        numEmployees: 1,
-        logoUrl: "http://c1.img",
-      },
-    });
+    const { company } = resp.body;
+    expect(company).toHaveProperty("handle", "c1");
+    expect(company.jobs[0]).toHaveProperty("title", "test job 1");
   });
 
   test("works for anon: company w/o jobs", async function () {
-    const resp = await request(app).get(`/companies/c2`);
+    const resp = await request(app).get(`/companies/c3`);
     expect(resp.body).toEqual({
       company: {
-        handle: "c2",
-        name: "C2",
-        description: "Desc2",
-        numEmployees: 2,
-        logoUrl: "http://c2.img",
-      },
+        handle: "c3",
+        name: "C3",
+        description: "Desc3",
+        numEmployees: 3,
+        logoUrl: "http://c3.img",
+        jobs: []
+      }
     });
   });
 
